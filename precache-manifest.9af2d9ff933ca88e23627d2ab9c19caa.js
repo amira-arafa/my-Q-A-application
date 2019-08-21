fe3a58d164e812cd33be217211fb1260d54eeeb9
amira7 <amiraarafa7@gmail.com>
2019-08-21 16:26:23 +0200

    Updates

---
+++ precache-manifest.9af2d9ff933ca88e23627d2ab9c19caa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe2d00e46da3bbe31e4f98b89662082",
+    "url": "/amira7/my-Q-A-application.git/index.html"
+  },
+  {
+    "revision": "9e91c2c6ac72b11ae1e9",
+    "url": "/amira7/my-Q-A-application.git/static/css/main.47a1063c.chunk.css"
+  },
+  {
+    "revision": "0ab8fedbce81d247dd70",
+    "url": "/amira7/my-Q-A-application.git/static/js/2.2c9a6bc0.chunk.js"
+  },
+  {
+    "revision": "9e91c2c6ac72b11ae1e9",
+    "url": "/amira7/my-Q-A-application.git/static/js/main.d045661d.chunk.js"
+  },
+  {
+    "revision": "5f3540dd434f2f1039cb",
+    "url": "/amira7/my-Q-A-application.git/static/js/runtime~main.f56c8105.js"
+  }
+]);
